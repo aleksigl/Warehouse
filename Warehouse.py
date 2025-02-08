a552f5b3c29b53a9f015e57ad8e91b34c3ac7258
aleksigl <112393919+aleksigl@users.noreply.github.com>
2025-02-08 19:31:42 +0100

    Add items list

--- Warehouse.py
+++ Warehouse.py
@@ -1,7 +1,20 @@
+items = []
+
+names = ["mini pump", "patch kit", "saddle bag"]
+quantities = [359, 777, 54]
+units = ["piece", "piece", "piece"]
+units_price = [22.5, 49.99, 250]
+for i in range(len(names)):
+    items = [{"name": names[i], "quantity": quantities[i], "unit": units[i], "unit_price": units_price[i]}]
+
 welcome = input("What would you like to do?")
 while welcome == "exit":
     print("Exiting! Bye...!")
     exit()
+if welcome == "show":
+    get_items()
+    welcome = input("What would you like to do next?")
 else:
     print("This is not a valid input.")
     welcome = input("Try again. What would you like to do?")
+
