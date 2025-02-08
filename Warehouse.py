d04f0a4122802435b4357ce86ae971309964a4c1
aleksigl <112393919+aleksigl@users.noreply.github.com>
2025-02-08 18:56:36 +0100

    Add welcoming input

--- Warehouse.py
+++ Warehouse.py
@@ -1,1 +1,7 @@
-input("What would you like to do?")
+welcome = input("What would you like to do?")
+while welcome == "exit":
+    print("Exiting! Bye...!")
+    exit()
+else:
+    print("This is not a valid input.")
+    welcome = input("Try again. What would you like to do?")
