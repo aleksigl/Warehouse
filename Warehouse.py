3811cd31eabadf008fdca492da1f05840ab4eb3d
aleksigl <112393919+aleksigl@users.noreply.github.com>
2025-02-08 22:45:44 +0100

    Add add_item function

--- Warehouse.py
+++ Warehouse.py
@@ -2,7 +2,7 @@ items = []
 
 names = ["mini pump", "patch kit", "saddle bag"]
 quantities = [359, 777, 54]
-units = ["piece", "piece", "piece"]
+units = ["pcs", "pcs", "pcs"]
 units_price = [22.5, 49.99, 250]
 
 for i in range(len(names)):
@@ -12,13 +12,33 @@ def get_items():
     print("\tName \t\t Quantity \t\t Unit \t\t Unit price (PLN)")
     print("-" * 84)
     for element in items:
-        item_details = (f"{element['name'].center(20)}\t{str('{:03d}'.format(element['quantity']).center(10))}\t\t{element['unit']}\t\t{str('{:.2f}'.format(element['unit_price']).center(15))}")
+        item_details = (f"{element['name'].center(20)}\t{str('{:03d}'.format(element['quantity']).center(10))}\t\t{element['unit'].center(7)}\t\t{str('{:.2f}'.format(element['unit_price']).center(15))}")
         print(item_details)
+
+def add_item():
+    print("Adding new item...")
+    while True:
+        i = str(input("Enter product's name: "))
+        element = {}              
+        if i.lower() == "quit":
+            print("You have quit.") 
+            break                          
+        element["name"] = i                           
+        element["quantity"] = int(input("Enter product's quantity: "))   
+        element["unit"] = str(input("Enter unit's type: "))
+        element["unit_price"] = float(input("Enter product's price per unit: "))
+        items.append(element)       
+        print()
+        print("You have succesfully added a new product to the warehouse stock.\nHere's current status:")        
+        get_items()
+        break
 
 welcome = input("What would you like to do?")
 while welcome != "exit":
     if welcome == "show":
         get_items()
+    elif welcome == "add":
+        add_item()
     else:
         print("This is not a valid input.")
     welcome = input("What would you like to do?")
